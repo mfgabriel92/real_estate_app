2544fe5b58cdc6853d57901caaf28935b22d9f14
Gabriel Monteiro Fernandes <mfgabriel92@gmail.com>
2024-12-22 09:58:11 +0800

    feat: fonts and assets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { Stack, SplashScreen } from "expo-router";
 import "./global.css";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [areFontsLoaded] = useFonts({
+    "Rubik-Bold": require("@/assets/fonts/Rubik-Bold.ttf"),
+    "Rubik-ExtraBold": require("@/assets/fonts/Rubik-ExtraBold.ttf"),
+    "Rubik-Light": require("@/assets/fonts/Rubik-Light.ttf"),
+    "Rubik-Medium": require("@/assets/fonts/Rubik-Medium.ttf"),
+    "Rubik-Regular": require("@/assets/fonts/Rubik-Regular.ttf"),
+    "Rubik-SemiBold": require("@/assets/fonts/Rubik-SemiBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (areFontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [areFontsLoaded]);
+
   return <Stack />;
 }
